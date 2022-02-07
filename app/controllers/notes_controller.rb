6a04bd69f822037a9ad9a16bce52cc6d2bde33a3
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-02-07 20:00:10 +0800

    Miniproject

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,12 +1,12 @@
 class NotesController < ApplicationController
-  before_action :set_note, only: %i[ show edit update destroy ]
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
 
-  # GET /notes or /notes.json
+  # GET /notes
   def index
     @notes = Note.all
   end
 
-  # GET /notes/1 or /notes/1.json
+  # GET /notes/1
   def show
   end
 
@@ -19,42 +19,30 @@ class NotesController < ApplicationController
   def edit
   end
 
-  # POST /notes or /notes.json
+  # POST /notes
   def create
     @note = Note.new(note_params)
 
-    respond_to do |format|
-      if @note.save
-        format.html { redirect_to note_url(@note), notice: "Note was successfully created." }
-        format.json { render :show, status: :created, location: @note }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    if @note.save
+      redirect_to @note, notice: 'Note was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /notes/1 or /notes/1.json
+  # PATCH/PUT /notes/1
   def update
-    respond_to do |format|
-      if @note.update(note_params)
-        format.html { redirect_to note_url(@note), notice: "Note was successfully updated." }
-        format.json { render :show, status: :ok, location: @note }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    if @note.update(note_params)
+      redirect_to @note, notice: 'Note was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /notes/1 or /notes/1.json
+  # DELETE /notes/1
   def destroy
     @note.destroy
-
-    respond_to do |format|
-      format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to notes_url, notice: 'Note was successfully destroyed.'
   end
 
   private
